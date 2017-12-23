4cc1deebd0975d86fe3a495707d4ae43558b8cdf
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-23 16:48:03 -0600

    initial

---
+++ src/main/java/com/marcuschiu/loadbalance/LoadBalanceApplication.java
@@ -0,0 +1,38 @@
+package com.marcuschiu.loadbalance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name="book-service", configuration = BookServiceConfiguration.class)
+@RestController
+public class LoadBalanceApplication {
+
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping("/hi")
+	public String hi() {
+		return this.restTemplate.getForObject("http://book-service/books/local-service-instance", String.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalanceApplication.class, args);
+	}
+}
+
